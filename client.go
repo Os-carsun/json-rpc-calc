5ce3ae5615176cb02666e5fc57cf717283792368
Os-carsun <good0991@gmail.com>
2018-04-29 04:22:39 -0700

    add client for test server functional

---
+++ client.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"./rpclib"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/rpc/jsonrpc"
+)
+
+func create(serverIP *string, serverPort *int) {
+	client, err := net.DialTimeout("tcp", fmt.Sprintf("%v:%v", *serverIP, *serverPort), 1000*1000*1000*30)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	c := jsonrpc.NewClient(client)
+
+	endChan := make(chan int, 15)
+
+	method := []string{"create", "set", "delete", "plus", "minus", "multiply", "divide"}
+	for i := 1; i <= 15; i++ {
+		request := &rpclib.RPCObj{
+			method[i%len(method)],
+			"1.0",
+			[]string{"test", "1234"},
+			i}
+
+		log.Println("client\t-", "call create method")
+
+		divCall := c.Go("Handler.Call", request, &rpclib.ReplyObj{}, nil)
+
+		go func(num int) {
+			reply := <-divCall.Done
+			obj := (reply.Reply).(*rpclib.ReplyObj)
+			log.Println("clent\t-", "recive response", reply.Reply, obj, num)
+			endChan <- num
+		}(i)
+	}
+
+	for i := 1; i <= 15; i++ {
+		_ = <-endChan
+	}
+}
+
+func main() {
+
+	serverIP := flag.String("IP", "127.0.0.1", "rpc serverIP, default is 127.0.0.1")
+	serverPort := flag.Int("port", 1234, "rpc port, default is 1234")
+	create(serverIP, serverPort)
+}
